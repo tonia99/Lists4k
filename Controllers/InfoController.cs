8daccd8c432639288981d1925cda44a12e9835ea
tonia99 <32978863+tonia99@users.noreply.github.com>
2019-04-03 21:44:31 +0300

    Add files via upload

---
+++ Controllers/InfoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Laba1FP.Controllers
+{
+    [Route("api/[controller]")]
+    public class InfoController : Controller
+    {
+        // GET
+        public IActionResult Get()
+        {
+            return View("index");
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Laba1FP.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult Get(List<int> firstArray, List<int> secondArray)
+        {
+            var result = string.Empty;
+            if (!firstArray.Any())
+            {
+                return new ContentResult
+                {
+                    Content = result,
+                    ContentType = "text/plain",
+                    StatusCode = 200
+                };
+            }
+
+            if (!secondArray.Any())
+            {
+                firstArray.ForEach(x => result += x.ToString());
+                return new ContentResult
+                {
+                    Content = result,
+                    ContentType = "text/plain",
+                    StatusCode = 200
+                };
+            }
+
+            var elements = firstArray.Where(x => Enumerable.Count(secondArray.Where(y => y == x)) == 2)
+                .Distinct()
+                .ToList();
+
+            
+            elements.ForEach(x => firstArray.RemoveAll(y => y == x));
+            firstArray.ForEach(x => result += x.ToString());
+            
+            
+
+            
+            
+            return new ContentResult
+            {
+                Content = result,
+                ContentType = "text/plain",
+                StatusCode = 200
+            };
+           
+        }
+        
+
+    }
+}
